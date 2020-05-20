2994880fed0567a2306e34dda542b3120c972700
luca2849 <luca@lucapassariello.co.uk>
2020-05-20 21:22:34 +0100

    Update for server

--- server.js
+++ server.js
@@ -84,7 +84,33 @@ io.on("connection", (socket) => {
 app.use("/", require("./routes/main"));
 app.use("/room", require("./routes/room"));
 
-const PORT = process.env.PORT || 5000;
-http.listen(PORT, () => {
-    console.log(`Server Listening on Port ${PORT}`);
+
+function wwwRedirect(req, res, next) {
+    if (req.headers.host.slice(0, 4) === 'www.') {
+        var newHost = req.headers.host.slice(4);
+        return res.redirect(301, req.protocol + '://' + newHost + req.originalUrl);
+    }
+    next();
+};
+
+app.set('trust proxy', true);
+app.use(wwwRedirect);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+module.exports = app;
