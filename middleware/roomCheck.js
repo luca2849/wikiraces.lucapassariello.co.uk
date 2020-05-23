3c84803de3fa27a2a1a4d71901384bb53890ebed
Luca Passariello <luca@lucapassariello.co.uk>
2020-05-23 15:56:32 +0100

    Responsive update to play page, removed Jumbotron background

--- middleware/roomCheck.js
+++ middleware/roomCheck.js
@@ -7,7 +7,7 @@ module.exports = async (req, res, next) => {
         });
         if (!room) {
             console.log("Room Not Found");
-            return red.redirect(`/room/join`);
+            return res.redirect(`/room/join`);
         }
         for (let i = 0; i < room.users.length; i++) {
             if (room.users[i].userId === req.session.userId) {
@@ -16,7 +16,7 @@ module.exports = async (req, res, next) => {
         }
     } catch (error) {
         console.error(error);
-        return red.redirect(`/room/join`);
+        return res.redirect(`/room/join`);
     }
     return res.redirect("/room/join");
 };
