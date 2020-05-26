1d9cc1b1b7953c217541a80dc69e06c61317f376
Luca Passariello <luca@lucapassariello.co.uk>
2020-05-26 17:21:04 +0100

    Fixed cookies banner bug

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,7 @@
-<script>
-    if (localStorage.getItem('cookieAck') != 'true') {
-        $('.cookieWarning').delay(500).fadeIn(300);
-    };
-    cookieAck = () => {
-        localStorage.setItem('cookieAck', 'true');
-        $('.cookieWarning').fadeOut(300);
-    }
-</script>
+if (localStorage.getItem("cookieAck") != "true") {
+    $(".cookieWarning").delay(500).fadeIn(300);
+}
+cookieAck = () => {
+    localStorage.setItem("cookieAck", "true");
+    $(".cookieWarning").fadeOut(300);
+};
