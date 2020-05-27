e498f207527ea05423b55e12829f535565a73c36
luca2849 <luca@lucapassariello.co.uk>
2020-05-27 16:49:09 +0100

    Temp. removed logging

--- middleware/accessLog.js
+++ middleware/accessLog.js
@@ -2,10 +2,10 @@ const util = require("../util");
 
 module.exports = async (req, res, next) => {
     try {
-        util.log(
-            `[${req.connection.remoteAddress}] -> [${req.originalUrl}]`,
-            "access.log"
-        );
+        //util.log(
+        //    `[${req.connection.remoteAddress}] -> [${req.originalUrl}]`,
+        //    "access.log"
+        //);
     } catch (error) {
         console.error(error);
     }
