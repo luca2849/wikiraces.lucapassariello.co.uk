95e7ab3c19155378e5e2e8992112d3b5d58e732a
Luca Passariello <luca@lucapassariello.co.uk>
2020-05-27 13:16:09 +0100

    Added privacy policy

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,8 @@
+// Check for cookie flag in localstorage
 if (localStorage.getItem("cookieAck") != "true") {
     $(".cookieWarning").delay(500).fadeIn(300);
 }
+// Cookie acknowledgement button callback
 cookieAck = () => {
     localStorage.setItem("cookieAck", "true");
     $(".cookieWarning").fadeOut(300);
